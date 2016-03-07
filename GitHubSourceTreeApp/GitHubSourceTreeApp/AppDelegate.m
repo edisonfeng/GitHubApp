2bdbfad0930a89bde8723005c3bbbd789eda5779
edisonfeng <edisonfeng1988@gmail.com>
2016-03-08 02:17:33 +0800

    yellowDev confirmed again

--- GitHubSourceTreeApp/GitHubSourceTreeApp/AppDelegate.m
+++ GitHubSourceTreeApp/GitHubSourceTreeApp/AppDelegate.m
@@ -16,7 +16,7 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    NSLog(@"yellowDev confirmed again");
     return YES;
 }
 
