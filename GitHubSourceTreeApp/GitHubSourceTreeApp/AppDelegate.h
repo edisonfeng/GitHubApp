2661f21d7ee2d74adc9f51f91a5d35bc1805355e
JamesCorp <edisonfeng1988@gmail.com>
2016-03-07 12:22:02 +0800

    新建工程GitHubSourceTreeApp

---
+++ GitHubSourceTreeApp/GitHubSourceTreeApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitHubSourceTreeApp
+//
+//  Created by JamesCorp on 16/3/7.
+//  Copyright © 2016年 JamesCorp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitHubSourceTreeApp/GitHubSourceTreeApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitHubSourceTreeApp
+//
+//  Created by JamesCorp on 16/3/7.
+//  Copyright © 2016年 JamesCorp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
